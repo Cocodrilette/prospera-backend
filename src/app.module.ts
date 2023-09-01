54a759625975804c30c3382e99425881278ad5f8
cocodrilette <cocodrilette@gmail.com>
2023-08-31 22:32:14 -0500

    Initial setup done

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.development.env',
+      isGlobal: true,
+    }),
+    MongooseModule.forRoot(process.env.MONGODB_URL, {
+      dbName: process.env.MONGODB_DB_NAME,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
