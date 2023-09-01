50c52e5295f9eb9cbc787a163fc3639d5de7f97b
cocodrilette <cocodrilette@gmail.com>
2023-08-31 23:30:26 -0500

    Feat: Paypal variables created

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -1,1 +1,13 @@
-export default () => ({});
+export default () => ({
+  providers: {
+    payment: {
+      paypal: {
+        clientId: process.env.PAYPAL_CLIENT_ID,
+        clientSecret: process.env.PAYPAL_CLIENT_SECRET,
+        baseUrl: process.env.PAYPAL_BASE_URL,
+        authUrl: process.env.PAYPAL_AUTH_URL,
+        ordersUrl: process.env.PAYPAL_ORDERS_URL,
+      },
+    },
+  },
+});
