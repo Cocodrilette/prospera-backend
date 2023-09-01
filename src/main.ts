25b5b19cd00f42b10da3755fc06c5f8b730e886e
cocodrilette <cocodrilette@gmail.com>
2023-08-31 23:49:47 -0500

    Feat: CORS set up

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,9 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+    origin: process.env.FRONTEND_URL,
+  });
   app.setGlobalPrefix('api/v1');
   app.useGlobalPipes(
     new ValidationPipe({
