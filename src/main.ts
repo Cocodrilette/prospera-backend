ab9c753e2b7991370264793446f2cb0857fa6525
cocodrilette <cocodrilette@gmail.com>
2023-11-14 23:56:17 -0500

    v0.1.3-experimental: allowd host array replaces by a regex

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,9 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import helmet from 'helmet';
-import { Serializer } from './common/adapter/serializer.adapter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
-  const serializer = new Serializer();
 
   app.use(
     helmet({
@@ -15,7 +12,7 @@ async function bootstrap() {
     }),
   );
   app.enableCors({
-    origin: [...serializer.env.parseArray(process.env.ALLOWED_ORIGINS)],
+    origin: process.env.ALLOWED_ORIGINS,
   });
   app.setGlobalPrefix('api/v1');
   app.useGlobalPipes(
